3f8be7b212563c0895104831385be8e4baeefbee
Debian <debian@debian10.linuxvmimages.local>
2021-03-09 17:54:06 -0600

    Nelli

--- homework.py
+++ homework.py
@@ -1,8 +1,5 @@
-def countsum(number):
-    if(number <1):
-        print ("enter other value")
-        return 0
-    else:
+def countsum(number): 
+
     if(number >1):
         a1 = 0
         a2 = 1
@@ -12,10 +9,12 @@ def countsum(number):
             k = a1 + a2
             a1 = a2
             a2 = k
-            print("sum of all fibonacci numbers less than entered is" sum)
+            print(sum)
             return sum
-    
-    def primeNumber(number):
+    else:
+        return 0
+
+def primeNumber(number):
         for i in range(2,int(number**0.5)+1):
             if number%i==0:
                 print ("the entered value is not prime")
@@ -24,12 +23,14 @@ def countsum(number):
             print ("entered value is prime")
             return True
 
-    def binary(number):
+def binary(number):
         if number >=1:
             binary(number//2)
             print (num%2)
 
-    def main():
+
+
+def main():
         while (True):
             N - int(input())
             if (countsum (number) != 1):
