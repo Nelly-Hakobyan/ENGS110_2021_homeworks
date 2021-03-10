e3b955033c0dc199082f0b186d3318a02f0ad16a
Debian <debian@debian10.linuxvmimages.local>
2021-03-10 07:17:27 -0600

    Nelli

--- homework.py
+++ homework.py
@@ -1,6 +1,7 @@
 def countsum(number): 
-
-    if(number >1):
+    if (number <= 0 ):
+        return 0
+    else:
         a1 = 0
         a2 = 1
         sum = 0
@@ -9,32 +10,41 @@ def countsum(number):
             k = a1 + a2
             a1 = a2
             a2 = k
-            print(sum)
-            return sum
-    else:
-        return 0
+        print(sum)
 
+       
+
+   
 def primeNumber(number):
-        for i in range(2,int(number**0.5)+1):
-            if number%i==0:
-                print ("the entered value is not prime")
-                return False
+    p = int(number**0.5)
+    for i in range (2, (p + 1)):
+        if number % i == 0:
+            print ("not prime")
+            return False
 
-            print ("entered value is prime")
-            return True
+        print ("prime")
+        return True 
+                  
+            
+
 
 def binary(number):
-        if number >=1:
+        if number > 0:
             binary(number//2)
-            print (num%2)
+            print (number%2, end=" ")
 
+                        
 
 
 def main():
-        while (True):
-            N - int(input())
-            if (countsum (number) != 1):
-                primeNumber(number)
-                binary(number)
-                break
+    while (True):
+        number =int(input("enter number:"))
+        if(countsum(number) != 1):
+            countsum (number)
+        primeNumber(number)
+        binary(number)
+       
+main()
+
+
 
